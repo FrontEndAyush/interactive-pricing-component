02605279b6034578642940d0aa0a110f589f442a
ayush <iamrealglider@gmail.com>
2024-07-11 23:59:59 +0530

    made on/off swtich

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [pageviews, setPageviews] = useState(100);
   const [price, setPrice] = useState(16);
+  const [isTrue, setIsTrue] = useState(true);
   const [isYearly, setIsYearly] = useState(false);
 
   useEffect(() => {
@@ -36,15 +37,40 @@ const App = () => {
                 $16.00 <span className="text-xl  font-light">/ month</span>
               </h1>
             </div>
-            <div className="flex justify-center ">
+            <div className="flex mt-4 justify-center ">
               <input
                 min={0}
                 max={100}
                 type="range"
                 name=""
-                className=" w-3/4 h-2 bg-[#A1F2EA]    appearance-none cursor-pointer"
+                className=" w-5/6 h-2 bg-[#A1F2EA]    appearance-none cursor-pointer"
                 id=""
               />
+            </div>
+
+            <div className="flex justify-center gap-2 py-12  ">
+              <div className="relative flex gap-2">
+                <h1 className="text-[#B6B9CA] text-[15px]">Monthly Billing</h1>
+                <div className="w-16 rounded-full hover:bg-[#7CE9DE] cursor-pointer duration-300 transition-all h-7 border bg-[#CFD8EE]"></div>
+                <p
+                  onClick={() => setIsTrue(false)}
+                  className={`h-5 w-5 bg-white absolute top-1  left-[109px] rounded-full ${
+                    isTrue == true ? "block" : "hidden"
+                  }`}
+                ></p>
+                <p
+                  onClick={() => setIsTrue(true)}
+                  className={`h-5 w-5 bg-white absolute top-1 left-[144px] rounded-full ${
+                    isTrue === false ? "block" : "hidden"
+                  } `}
+                ></p>
+              </div>
+              <div className="flex gap-2">
+                <p className="text-[#B6B9CA] text-[15px]">Yearly Billing</p>
+                <p className="text-[11px]  bg-[#FFECE7] text-[#F3B29B] font-semibold rounded-full p-1 border ">
+                  25% discount
+                </p>
+              </div>
             </div>
           </div>
         </div>
